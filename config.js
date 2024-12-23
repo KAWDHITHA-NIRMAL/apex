0170bb1ca00caed6ce123a2177cf359f201e88d3
‌‌‌‌✘ 𝗞𝗔𝗪𝗗𝗛𝗜𝗧𝗛𝗔 𝗡𝗜𝗥𝗠𝗔𝗟 •| <141761993+KAWDHITHA-NIRMAL@users.noreply.github.com>
2024-12-23 14:24:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "hfYTAALT#AgtpE2RDUQ_o_YWHiU9N3WOgpu3rsjq4AwGclRFIxgc",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/198a322e8cc0f2bd6cba2.jpg",
-ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi Apex-MD Is Online Now 💻\n*💻 Owner* - Apex-MD\n\n*💻 Owner Number* -94765527900",
-SUDO_NB: process.env.SUDO_NB || "94718913389",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi QUEEN-KYLIE-MD Is Online Now 💻\n*💻 Owner* - QUEEN-KYLIE-MD\n\n*💻 Owner Number* -94765527900",
+SUDO_NB: process.env.SUDO_NB || "94729787750",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
 MODE: process.env.MODE || "public",
-AUTO_VOICE:"true"
+AUTO_VOICE:"false"
 };
