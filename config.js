17090b3f23accd2e50b0871a443f368dadf273f2
‌‌‌‌✘ 𝗞𝗔𝗪𝗗𝗛𝗜𝗧𝗛𝗔 𝗡𝗜𝗥𝗠𝗔𝗟 •| <141761993+KAWDHITHA-NIRMAL@users.noreply.github.com>
2024-12-23 13:47:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "whfYTAALT#AgtpE2RDUQ_o_YWHiU9N3WOgpu3rsjq4AwGclRFIxgc",
+SESSION_ID: process.env.SESSION_ID || "hfYTAALT#AgtpE2RDUQ_o_YWHiU9N3WOgpu3rsjq4AwGclRFIxgc",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/98XnsZL/20241008-150032.png",
 ALIVE_MSG: process.env.ALIVE_IMG || "hi",
 SUDO_NB: process.env.SUDO_NB || "94729787750",
