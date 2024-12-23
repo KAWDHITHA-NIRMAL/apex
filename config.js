3246c74b062cda38cd7178e522c6e2a103393437
‌‌‌‌✘ 𝗞𝗔𝗪𝗗𝗛𝗜𝗧𝗛𝗔 𝗡𝗜𝗥𝗠𝗔𝗟 •| <141761993+KAWDHITHA-NIRMAL@users.noreply.github.com>
2024-12-23 15:51:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "hfYTAALT#AgtpE2RDUQ_o_YWHiU9N3WOgpu3rsjq4AwGclRFIxgc",
+SESSION_ID: process.env.SESSION_ID || "wH5ixTxa#sx2cr5qZpW8PlFzkDnil2flVqsQa7UblDlCELKBRzv0",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/198a322e8cc0f2bd6cba2.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi Apex-MD Is Online Now 💻\n*💻 Owner* - Apex-MD\n\n*💻 Owner Number* -94765527900",
 SUDO_NB: process.env.SUDO_NB || "94718913389",
